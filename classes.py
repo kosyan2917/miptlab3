9a2c2652dee1e4cd2ce8c2590a1e00506376ea74
kosyan2917 <pevetz@bk.ru>
2019-10-10 08:46:00 +0300

    pep8

--- classes.py
+++ classes.py
@@ -1,37 +1,39 @@
 import random
+
+
 class Cat:
     def __init__(self):
         pass
-    
+
     def __str__(self):
         return self.name
 
-    def sleep(self,name):
+    def sleep(self, name):
         print("{0} спит".format(self.name))
-        
+
     def meow(self, name):
         print("{0} орет: МУЦУРАЕВА ХАЧЮ!".format(self.name))
-            
+
     def get_name(self):
         try:
             print(self.name)
         except AttributeError:
             print("Daite imya koty!")
+
     def set_name(self, name):
         self.name = name
-    
+
     def __add__(self, name):
-        
+
 
 a = [Cat(), Cat(), Cat(), Cat()]
-b=1
+b = 1
 for i in a:
     i.set_name('Номер {0}'.format(b))
-    b+=1
+    b += 1
     i.get_name()
 
-for i in range(0,5):
-    b = random.randint(0,2)      
-    c = a[random.randint(0,3)]
+for i in range(0, 5):
+    b = random.randint(0, 2)
+    c = a[random.randint(0, 3)]
     if b == 0:
-        
