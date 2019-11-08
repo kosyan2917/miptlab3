115406f8c1e0e8e662336d70773ff0138d6b69b9
kosyan2917 <pevetz@bk.ru>
2019-11-08 16:06:38 +0300

    Zvezda1.py

--- Zvezda1.py
+++ Zvezda1.py
@@ -15,8 +15,7 @@ class Players:
         self.vx = -2
 
     def create_player1(self):
-        self.obj = [self.game.objects.append(self.game.canv.create_oval(500, 2000, 520, 1980, fill='black')), self.game.objects.append(self.game.canv.create_oval(530, 2000, 550, 1980, fill='black')), self.game.objects.append(self.game.canv.create_rectangle(500, 1985, 550, 1970, fill='red'))]
-        pass
+        self.obj = [self.game.canv.create_oval(500, 2000, 520, 1980, fill='black'), self.game.canv.create_oval(530, 2000, 550, 1980, fill='black'), self.game.canv.create_rectangle(500, 1985, 550, 1970, fill='red')]
 
 class Camera:
     def __init__(self):
@@ -27,16 +26,16 @@ class Camera:
         self.vx = 0
         self.vy = 0
     def set_x_speedmore(self, event):
-        self.vx+=2
+        self.vx += 2
     
     def set_y_speedmore(self, event):
-        self.vy+=2
+        self.vy += 2
 
     def set_x_speedless(self, event):
-        self.vx-=2
+        self.vx -= 2
         
     def set_y_speedless(self, event):
-        self.vy-=2
+        self.vy -= 2
 
 
 class Mount:
@@ -63,7 +62,7 @@ class Game:
         self.canv.pack(fill=tk.BOTH, expand=1)
 
     def tick(self):
-        print([self.camera.x1,self.camera.x2,self.camera.y1,self.camera.y2])
+        #print([self.camera.x1,self.camera.x2,self.camera.y1,self.camera.y2])
         self.camera.x1 += self.camera.vx
         self.camera.x2 += self.camera.vx
         self.camera.y1 += self.camera.vy
@@ -84,25 +83,40 @@ class Game:
             self.camera.vy = 0
             self.camera.y1 = 1280
             self.camera.y2 = 2000
-        for object in self.objects:
-            coords = self.canv.coords(object.obj)
-            new_coords = []
-            for i in range(0,len(coords)):
-                if i%2==0:
-                    new_coords.append(coords[i] - self.camera.vx)
-                else:
-                    new_coords.append(coords[i] - self.camera.vy)
-            self.canv.coords(object.obj, new_coords)
 
+        for gameobj in self.objects:
+            coords = self.canv.coords(gameobj.obj)
+            if isinstance(gameobj, Players):
+                print(gameobj)
+                print(gameobj.obj)
+                for j in range(0, len(gameobj.obj)):
+                    coords = self.canv.coords(gameobj.obj[j])
+                    new_coords = []
+                    for i in range(0, len(coords)):
+                        if i % 2 == 0:
+                            new_coords.append(coords[i] - self.camera.vx)
+                        else:
+                            new_coords.append(coords[i] - self.camera.vy)
+                    self.canv.coords(gameobj.obj[j], new_coords)
+            else:
+                new_coords = []
+                coords = self.canv.coords(gameobj.obj)
+                for i in range(0,len(coords)):
+                    if i%2==0:
+                        new_coords.append(coords[i] - self.camera.vx)
+                    else:
+                        new_coords.append(coords[i] - self.camera.vy)
+                self.canv.coords(gameobj.obj, new_coords)
         self.root.after(10, self.tick)
 
     def run(self):
         #self.objects.append(self.canv.create_line(0, 0, 3000, 0))
         #self.objects.append(self.canv.create_line(3000, 0, 3000, 2000))
         #self.objects.append(self.canv.create_line(0, 0, 100, 50))
-        player1 = Players(self)
-        player2 = Players(self)
-        player1.create_player1()
+        self.player1 = Players(self)
+        self.objects.append(self.player1)
+        self.player2 = Players(self)
+        self.player1.create_player1()
         for i in range(0,20):
             self.objects.append(Mount(self))
         print(self.objects)
